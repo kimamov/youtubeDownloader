e3e996e7ef79420e51e64c5bd1bea32517c062f1
Nyannazz <kantemir.imam@gmail.com>
2019-04-30 17:37:38 +0200

    calling the api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+        videoLink: ''
+    }
+  }
+  onChange=(event)=>{
+    event.preventDefault()
+    const name=event.target.name
+    this.setState({[name]:event.target.value})
+  }
+  getVideo=(event)=>{
+    event.preventDefault()
+    let dlWindow=window.open(`http://localhost:5000/audio?videolink=${this.state.videoLink}`)
+    setTimeout(()=>{window.close(dlWindow)},8000)
+    
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.videoLink}
+        </header>
+        <form onSubmit={this.getVideo}>
+          <input name='videoLink' type='text' onChange={this.onChange}></input>
+          <input type="submit"></input>
+      </form>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
