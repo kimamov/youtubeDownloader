fd10121cc1e208a9bee0da15666ba9ccfa495228
Nyannazz <kantemir.imam@gmail.com>
2019-05-03 19:04:06 +0200

    scss set up and additional ui

---
+++ asd.js
@@ -0,0 +1,5 @@
+const url="https://r5---sn-8xgn5uxa-cxge.googlevideo.com/videoplayback?id=o-AB9SI2vbI79Zu3M2BzAhBoyXbg8T0AA5CsZ8Tq4OSTsM&itag=22&source=youtube&requiressl=yes&mm=31%2C29&mn=sn-8xgn5uxa-cxge%2Csn-4g5ednss&ms=au%2Crdu&mv=m&pcm2cms=yes&pl=24&ei=tAjMXI76G4P61gK6rJOwCA&initcwndbps=1608750&mime=video%2Fmp4&ratebypass=yes&dur=1201.934&lmt=1555812612964331&mt=1556875368&fvip=5&c=WEB&txp=5535432&ip=87.123.192.9&ipbits=0&expire=1556897044&sparams=ip%2Cipbits%2Cexpire%2Cid%2Citag%2Csource%2Crequiressl%2Cmm%2Cmn%2Cms%2Cmv%2Cpcm2cms%2Cpl%2Cei%2Cinitcwndbps%2Cmime%2Cratebypass%2Cdur%2Clmt&signature=41FA8F4C74F4187EA662F940CE601A58A3D113BE.46ACB1844E58CDA629A5A60C05B28DD1BD27C694&key=yt8";
+
+let arr=url.split('&')
+
+arr.forEach(element=>{console.log(element)})
---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchBar = (props) => {
+  return (
+    <form id='videoSearchForm' onSubmit={props.getVideoInfo}>
+        <div>
+            <input placeholder='youtube video url' name='videoLink' type='text' onChange={props.onChange}></input>
+            <input className='roundedButton' type="submit"></input>
+        </div>       
+    </form>
+  )
+}
+
+export default SearchBar
+
