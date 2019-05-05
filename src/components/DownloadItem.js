0e06ba8275dbfe6a34ded28d352e3684e1579671
Nyannazz <kantemir.imam@gmail.com>
2019-05-05 19:05:14 +0200

    mobile fixes and build

---
+++ src/components/DownloadItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DownloadItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DownloadItem
---
+++ src/components/DownloadList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+const DownloadList = (props) => {
+  return (
+    <div className={'downloadList'}>
+        <Link 
+          to='..'
+          className={'downloadListToggle undecoratedLink'}        >
+          <p>OPEN</p>
+        </Link>
+        <div className={'downloadListHeader'}>
+            <h1>LIST OF FORMATS</h1>
+            <p>SAVE AS FROM HERE</p>
+        </div>
+        <div className={'listContainer'}>
+            {(typeof props.videoSelect.formats =="object")&&props.videoSelect.formats.map((item,index)=>
+            <div value={index}>
+                <p>{'type: '+item.type}</p>
+                <p>{' quality: '+ item.quality+ ' '+(item.videoOnly?'only video':'')}</p>
+                <a href={item.url} Download>VIDEO</a>
+            </div>
+            )}
+        </div>    
+    </div>
+  )
+}
+
+export default DownloadList
